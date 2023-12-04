d7b2230e18c183887dd31e123914aaf29c986cb1
Joel Sanchez <joelalejandro.development@gmail.com>
2023-12-04 10:41:17 -0400

    estructura basica y maquetado de login

---
+++ scripts/base.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    fetch('/components/header/header.html')
+          .then(response => response.text())
+          .then(html => {
+            document.body.insertAdjacentHTML('afterbegin', html);
+
+            let newCSS = document.createElement('link');
+            newCSS.rel = 'stylesheet';
+            newCSS.href = '/components/header/header.css';
+            document.head.appendChild(newCSS);
+            $(".toggle-header").on("click",()=>{
+                $(".header nav").toggleClass("visible")
+            })
+            $(document).on("keydown",(e)=>{
+                if (e.key == "Escape") {
+                    $(".header nav").removeClass("visible")
+                }
+            })
+        })
+    
+    fetch('/components/footer/footer.html')
+        .then(response => response.text())
+        .then(html => {
+            document.body.insertAdjacentHTML('beforeend', html);
+        
+            let newCSS = document.createElement('link');
+            newCSS.rel = 'stylesheet';
+            newCSS.href = '/components/footer/footer.css';
+            document.head.appendChild(newCSS);
+        });
+        
+})
